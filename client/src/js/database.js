75dff51a927deacb2c768b5636d311b7407c2029
unknown <youssef.ojeil@hotmail.com>
2022-11-30 18:58:41 -0500

    setup getdb function to get all content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   try {
-    const db = await openDB('jate, 1');
+    console.log('PUT to the database');
+    const db = await openDB('jate', 1);
     const tx = db.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
     const request = store.put({ id: 1, value: content });
@@ -33,6 +34,19 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('GET all from the database');
+    const todosDb = await openDB('todos', 1);
+    const tx = todosDb.transaction('todos', 'readonly');
+    const store = tx.objectStore('todos');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (error) {
+    console.error('getDb not implemented', error);
+  }
+};
 
 initdb();
