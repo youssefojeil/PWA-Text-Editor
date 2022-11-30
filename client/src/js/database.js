1d597d7c821bc57b52745c4b46db5cc4fa5e5050
unknown <youssef.ojeil@hotmail.com>
2022-11-30 18:42:53 -0500

    setup putdb logic to add content to the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate, 1');
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    
+    console.log(request);
+    const result = await request;
+
+    console.log(`${result} saved to the database`);
+    return result;
+    
+  } catch (error) {
+    console.error('putDb not implemented', error);    
+  }
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
 
 module.exports = () => {
   return {
@@ -24,7 +23,10 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW()      
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),       
     ],
 
     module: {
